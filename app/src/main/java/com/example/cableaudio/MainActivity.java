99d5ea37cd44e05f62a072c9e90fe6af723c0d86
Juanpax68 <jg4377395@gmail.com>
2024-08-28 20:06:10 -0300

    avance del proyecto

---
+++ app/src/main/java/com/example/cableaudio/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.cableaudio;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+private TextView tex1,tex2,tex3,tex4,tex5,tex6;
+private Button btn1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        tex1 = findViewById(R.id.inicio);
+        tex2 = findViewById(R.id.nombre);
+        tex3 = findViewById(R.id.contra);
+        tex4 = findViewById(R.id.cuenta);
+        tex5 = findViewById(R.id.registro);
+        tex6 = findViewById(R.id.olvidar);
+        btn1 = findViewById(R.id.ingreso);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void nuevo(View view){
+        Intent nuevoo = new Intent(this, nuevo_user.class);
+        startActivity(nuevoo);
+    }
+    public void contra(View view){
+        Intent contraaa = new Intent(this, olvido_contra.class);
+        startActivity(contraaa);
+    }
+}
---
+++ app/src/main/java/com/example/cableaudio/olvido_contra.java
@@ -0,0 +1,39 @@
+package com.example.cableaudio;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class olvido_contra extends AppCompatActivity {
+private TextView t1,t2;
+private Button bo1,bo2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_olvido_contra);
+
+        t1 = findViewById(R.id.correo_elec);
+        t2 = findViewById(R.id.mensaje_mo);
+        bo1 = findViewById(R.id.restablecer_contra);
+        bo2 = findViewById(R.id.volver_rr);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void volve(View view){
+        Intent vol = new Intent(this, MainActivity.class);
+        startActivity(vol);
+    }
+}
